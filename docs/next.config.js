a822a616bea784619f452999c367e211e37fa931
Sergio <77530549+sergi0g@users.noreply.github.com>
2024-07-17 14:28:49 +0300

    Add documentation

---
+++ docs/next.config.js
@@ -0,0 +1,16 @@
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+});
+
+module.exports = withNextra(
+  {
+    output: "export",
+    images: {
+      unoptimized: true
+    }
+  }
+);
+
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
---
+++ docs/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "theme.config.jsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
