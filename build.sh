b5ebb33627d3907300b5f58a8a05d8f2880f905f
Sergio <77530549+sergi0g@users.noreply.github.com>
2024-09-06 21:46:43 +0300

    Optimize workflows (#25)
    feat: optimize workflows
    
    ---------
    
    Co-authored-by: Stavros <steveiliop56@gmail.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-rm -rf src/static
+# This is kind of like a shim that makes sure the frontend is rebuilt when running a build. For example you can run `./build.sh cargo build --release`
+
+# Frontend
 cd web/
+
+# Build
 bun run build
+
+# Copy UI to src folder
 cp -r dist/ ../src/static
-cargo build $@
+
+# Run command from argv
+
+$@
