6d1b5d339af6f5a6605a48c6eaadb635da212039
Sergio <77530549+sergi0g@users.noreply.github.com>
2024-09-07 11:03:53 +0300

    Remove colon from last checked

--- web/src/components/LastChecked.tsx
+++ web/src/components/LastChecked.tsx
@@ -2,5 +2,5 @@ import { intlFormatDistance } from "date-fns/intlFormatDistance";
 
 export function LastChecked({ datetime }: { datetime: string }) {
   const date = intlFormatDistance(new Date(datetime), new Date());
-  return <h3>Last checked: {date}</h3>;
+  return <h3>Last checked {date}</h3>;
 }
